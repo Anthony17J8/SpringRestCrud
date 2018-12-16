0683af6b5750889afbbca0b1481bc08469f72dcc
Kiselev <j8kiselev@gmail.com>
2018-12-16 13:36:43 +0300

    Adding request mapping for getting a single customer

--- src/main/java/ru/icoltd/springdemo/rest/CustomerRestController.java
+++ src/main/java/ru/icoltd/springdemo/rest/CustomerRestController.java
@@ -2,6 +2,7 @@ package ru.icoltd.springdemo.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.icoltd.springdemo.entity.Customer;
@@ -22,5 +23,14 @@ public class CustomerRestController {
     public List<Customer> getCustomers() {
 
         return customerService.getCustomers();
+    }
+
+    // add mapping for GET /customers/{customerId}
+    @GetMapping("/customers/{customerId}")
+    public Customer getCustomer(@PathVariable int customerId) {
+
+        Customer theCustomer = customerService.getCustomer(customerId);
+
+        return theCustomer;
     }
 }
